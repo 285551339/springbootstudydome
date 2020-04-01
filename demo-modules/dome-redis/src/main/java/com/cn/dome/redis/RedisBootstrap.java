6dcf1c405a753d326d1a9e6116a1c5149f7cea7e
zangliulu <zangliulu@sdxnetcafe.com>
2020-04-01 15:15:53 +0800

    整理jar

--- demo-modules/dome-redis/src/main/java/com/cn/dome/redis/RedisBootstrap.java
+++ demo-modules/dome-redis/src/main/java/com/cn/dome/redis/RedisBootstrap.java
@@ -1,11 +1,13 @@
 package com.cn.dome.redis;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description
  * @ClassName RedisBootstrap
  * @Author zangliulu
  * @date 2020.04.01 14:41
  */
-
+@SpringBootApplication
 public class RedisBootstrap {
 }
