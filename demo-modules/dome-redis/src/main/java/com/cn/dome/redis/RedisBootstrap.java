4d9209a3cd39617fd48e2100c9998202db88ee8c
zangliulu <zangliulu@sdxnetcafe.com>
2020-04-01 15:11:42 +0800

    redis 测试dome创建

---
+++ demo-modules/dome-redis/src/main/java/com/cn/dome/redis/RedisBootstrap.java
@@ -0,0 +1,11 @@
+package com.cn.dome.redis;
+
+/**
+ * @Description
+ * @ClassName RedisBootstrap
+ * @Author zangliulu
+ * @date 2020.04.01 14:41
+ */
+
+public class RedisBootstrap {
+}
---
+++ dome-common/src/test/java/com/cn/dome/common/DemoCommonApplicationTests.java
@@ -0,0 +1,10 @@
+package com.cn.dome.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCommonApplicationTests {
+
+
+
+}
