a4e68b49f6b7157335aa7d83e3c539df4adf9f2d
zangliulu <il>
2020-04-03 11:27:12 +0800

    测试

--- demo-modules/dome-redis/src/main/java/com/cn/dome/redis/RedisApplication.java
+++ demo-modules/dome-redis/src/main/java/com/cn/dome/redis/RedisApplication.java
@@ -16,5 +16,6 @@ public class RedisApplication {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TestConfig.class);
+        System.currentTimeMillis();
     }
 }
