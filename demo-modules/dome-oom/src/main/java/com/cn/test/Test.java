87fcbe9bf17cbbfb402f054fb0cdbda075574c80
zangliulu <zangliulu@qq.com>
2020-05-29 17:44:18 +0800

    OOM测试

---
+++ demo-modules/dome-oom/src/main/java/com/cn/test/Test.java
@@ -0,0 +1,38 @@
+package com.cn.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.WeakHashMap;
+
+/**
+ * @Description
+ * @ClassName Test
+ * @Author zangliulu
+ * @date 2020.05.26 10:05
+ */
+public class Test {
+
+    private static  int _1M=1024*1204;
+
+    /*static class OOMObject{
+    }*/
+
+    public static void main(String[] args) {
+      /*  List <OOMObject> listOmm= new ArrayList();
+        while (true){
+            listOmm.add(new OOMObject());
+        }*/
+    }
+
+    /**
+     *功能描述 测试jvm
+     * @author zangliulu
+     * @date 2020/5/28
+     * @param  * @param
+     * @return void
+     */
+    public static void testPretenureSizeThreshold(){
+
+    }
+
+}
